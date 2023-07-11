a2ab2f58d4042c0e8d637f226f318cbbafbd1b4e
Dileep Kishore <k.dileep1994@gmail.com>
2023-07-11 10:16:45 -0400

    Add: Script to aggregrate results from multiple genomes

---
+++ aggregate_results.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+import argparse
+import pathlib
+from typing import List
+
+import pandas as pd
+
+def main(genome_paths: List[pathlib.Path], output_dir: pathlib.Path):
+    print(output_dir)
+    partial_results = []
+    genomes = [genome_path.stem for genome_path in genome_paths]
+    for outputs in output_dir.iterdir():
+        if outputs.is_dir() and outputs.stem in genomes:
+            print("Processing results from " + str(outputs))
+            partial_results_file = outputs / "prediction_results.csv"
+            partial_results.append(pd.read_csv(partial_results_file))
+    results = pd.concat(partial_results)
+    results_file_csv = output_dir / "aggregated_results.csv"
+    results_file_html = output_dir / "aggregated_results.html"
+    results.to_csv(results_file_csv, sep=",", index=False)
+    results.to_html(results_file_html, index=False)
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument(
+        "genomes", help="File paths of the genomes used for BGC function prediction",
+        nargs="+",
+    )
+    PARSER.add_argument(
+        "--output_dir", help="Directory for the output files", default="outputs"
+    )
+    ARGS = PARSER.parse_args()
+
+    genome_paths = [pathlib.Path(genome) for genome in ARGS.genomes]
+    output_dir = pathlib.Path(ARGS.output_dir)
+    main(genome_paths, output_dir)
