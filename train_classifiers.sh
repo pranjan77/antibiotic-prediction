a888da76ec79bdb1ef31f56edf8113f6d169acc0
Dileep Kishore <k.dileep1994@gmail.com>
2023-07-11 11:55:07 -0400

    Add: Classifiers without SSN features

--- train_classifiers.sh
+++ train_classifiers.sh
@@ -2,5 +2,6 @@
 
 python train_classifiers.py "data/test/inputs/3,7-dihydroxytropolone.gbk" "data/test/inputs/3,7-dihydroxytropolone.txt" \
     --output "data/test" \
+    --no_SSN "True" \
     --antismash_version 5 \
     --rgi_version 5
