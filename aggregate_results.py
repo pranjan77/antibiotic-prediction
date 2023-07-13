8f909da530cb1444a380d09c13982f0b8320b18d
Dileep Kishore <k.dileep1994@gmail.com>
2023-07-13 15:30:26 -0400

    Change: Update format of aggregated results

--- aggregate_results.py
+++ aggregate_results.py
@@ -7,6 +7,34 @@ from typing import List
 import pandas as pd
 
 
+def update_results(results: pd.DataFrame):
+    # Step1: Fitler results
+    results_filtered = results[~results.function.isin({"antitumor", "antieuk"})]
+    # Step 2: Update dataframe
+    data_dict = dict()
+    regions = set()
+    for _, row in results_filtered.iterrows():
+        if row.region not in regions:
+            regions.add(row.region)
+            data_item = dict()
+            data_item["Genome"] = row.genome
+            data_item["Region"] = row.region
+            data_item[row.function] = row.probability
+            data_dict[row.region] = data_item
+        else:
+            data_dict[row.region][row.function] = row.probability
+    columns = [
+        "Genome",
+        "Region",
+        "antibacterial",
+        "antifungal",
+        "antigrampos",
+        "antigramneg",
+    ]
+    results_updated = pd.DataFrame(data_dict.values())[columns]
+    return results_updated
+
+
 def main(genome_paths: List[pathlib.Path], output_dir: pathlib.Path):
     partial_results = []
     genomes = [genome_path.stem for genome_path in genome_paths]
@@ -16,10 +44,11 @@ def main(genome_paths: List[pathlib.Path], output_dir: pathlib.Path):
             partial_results_file = outputs / "prediction_results.csv"
             partial_results.append(pd.read_csv(partial_results_file))
     results = pd.concat(partial_results)
+    results_updated = update_results(results)
     results_file_csv = output_dir / "aggregated_results.csv"
     results_file_html = output_dir / "aggregated_results.html"
-    results.to_csv(results_file_csv, sep=",", index=False)
-    results.to_html(results_file_html, index=False)
+    results_updated.to_csv(results_file_csv, sep=",", index=False)
+    results_updated.to_html(results_file_html, index=False)
 
 
 if __name__ == "__main__":
